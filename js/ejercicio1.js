614dfc3cb5620731ba0be94441c432c22461e59e
HenryTito <henry.jl.live@gmail.com>
2022-02-22 15:49:49 -0500

    Estructura basica del proyecto

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+document.getElementById("tipo1").innerHTML = "Ejemplo del tipo 1 de Output";
+document.write("Ejemplo de texto con document.write");
+
+window.alert("Ejemplo de output con window.alert");
+
+console.log("HOLA MUNDO");
