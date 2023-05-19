e037736d2da5aea6808fc5bb0b2bd2c8d22c319a
Dilan1502 <dbb.haro@yavirac.edu.ec>
2023-05-18 22:38:41 -0500

    cambioramita

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,6 +2,8 @@ document.getElementById("tipo1").innerHTML = "Ejemplo del tipo 1 de Output";
 let dinero = 10;
 const nombre = "dilan";
 let compra = 4;
+const venta = 6;
+document.write("texto de ejemplo");
 document.write("Ejemplo de texto con document.write");
 
 window.alert("Ejemplo de output con window.alert");
