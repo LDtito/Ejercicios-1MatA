af5c58e187cede9da14977116f9210eee336cede
Dilan1502 <dbb.haro@yavirac.edu.ec>
2023-05-18 22:24:04 -0500

    cambiostres

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,5 +1,6 @@
 document.getElementById("tipo1").innerHTML = "Ejemplo del tipo 1 de Output";
 let dinero = 10;
+document.write("este es una prueba");
 document.write("Ejemplo de texto con document.write");
 
 window.alert("Ejemplo de output con window.alert");
